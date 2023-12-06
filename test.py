3bf13f73b4a39fb6a532d74b192e1cea2af16f67
Karla:b <149977397+ProgrammerBearCoffe@users.noreply.github.com>
2023-12-05 20:27:27 -0600

    Update test.py
    cuchao

--- test.py
+++ test.py
@@ -2,10 +2,27 @@ import mysql.connector
 
 cnn = mysql.connector.connect(host="localhost", user="root", passwd="karla", database="amazon")
 
-cur = cnn.cursor()
-cur.execute("SELECT * FROM usuario")
-datos = cur.fetchall()
+#def usuarios_panolis():
+ #   cur = cnn.cursor()
+  #  cur.execute("SELECT * FROM usuario")
+   # datos = cur.fetchall()
+   # cur.close()
+   # cnn.close()
+    #return datos
 
-for fila in datos:
-    print(fila)
+def insertar_panolis(nombre, email, telefono, sexo, fecha_nacimiento, contrasena, subtotal, cantidad_productos):
+    cur = cnn.cursor()
+    sql = "INSERT INTO `usuario` (`nombre`, `email`, `telefono`, `sexo`, `fecha_nacimiento`, `contrasena`, `subtotal`, `cantidad_productos`) VALUES('{}', '{}', '{}', '{}', '{}', '{}', {}, {});".format(
+        nombre, email, telefono, sexo, fecha_nacimiento, contrasena, subtotal, cantidad_productos)
+    cur.execute(sql)
+    n = cur.rowcount
+    cnn.commit()
+    cur.close()
+    return n
+
+print(insertar_panolis('Luisa Martínez', 'luisa@example.com', '1239876543', 'F', '1992-07-18', 'pass123', 160.75, 5))
+
+#datos=usuarios_panolis()
+#for fila in datos:
+ #   print(fila)
 #print(cnn)
